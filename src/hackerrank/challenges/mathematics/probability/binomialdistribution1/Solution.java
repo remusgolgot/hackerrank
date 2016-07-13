5cf0ef9460f098acbd1f3e8d71cd849681815304
Remus Golgot <remus.golgot@arobs.com>
2016-07-13 15:45:42 +0300

    add solution for binomial distribution 2

--- src/hackerrank/challenges/mathematics/probability/binomialdistribution1/Solution.java
+++ src/hackerrank/challenges/mathematics/probability/binomialdistribution1/Solution.java
@@ -3,25 +3,27 @@ package hackerrank.challenges.mathematics.probability.binomialdistribution1;
 /**
  * https://www.hackerrank.com/challenges/binomial-distribution-1
  *
- A blindfolded marksman finds that on the average he hits the target 4 times out of 5. If he fires 4 shots, what is the probability of
+ A blindfolded marksman finds that on the average he hits the target 4 times out of 5.
+
+ If he fires 4 shots, what is the probability of :
  a) more than 2 hits?
  b) at least 3 misses?
  */
 
 public class Solution {
 
-    /**     *
+    /**
 
      ph = 0.800
      pm = 0.200
 
-     p4 hits : 0.4096  ( ph ^ 4 )
-     p3 hits : 0.4096  ( 4 * ph * ph * ph * pm )
-     p2 hits : 0.1536  ( 6 * ph * ph * pm * pm )
-     p1 hit  : 0.0256  ( 4 * ph * pm * pm * pm )
-     p0 hits : 0.0016
+     p (4 hits) : 0.4096  ( ph ^ 4 )
+     p (3 hits) : 0.4096  ( 4 * ph * ph * ph * pm )
+     p (2 hits) : 0.1536  ( 6 * ph * ph * pm * pm )
+     p (1 hit)  : 0.0256  ( 4 * ph * pm * pm * pm )
+     p (0 hits) : 0.0016  ( pm ^ 4 )
 
-     a) p(hits > 2) = p3 + p4 = 0.8192
-     b) hits <= 1   = p0 + p1 = 0.0272
+     a) p(hits > 2)  = p3 + p4 = 0.8192
+     b) p(hits <= 1) = p0 + p1 = 0.0272
      */
 }
---
+++ src/hackerrank/challenges/mathematics/probability/binomialdistribution2/Solution.java
@@ -0,0 +1,27 @@
+package hackerrank.challenges.mathematics.probability.binomialdistribution2;
+
+/**
+ * https://www.hackerrank.com/challenges/binomial-distribution-2
+ *
+ The ratio of boys to girls born in Russia is 1.09:1.
+ What proportion of Russian families with exactly 6 children will have at least 3 boys? (Ignore the probability of multiple births.)
+ */
+public class Solution {
+
+   /**
+   p(boy)  = 0.5215
+   p(girl) = 0.4784
+
+   0 boys = 0.0119
+   1 boy = 0.0784
+   2 boys = 0.2136
+   3 boys = 0.3105
+   4 boys = 0.2539
+   5 boy  = 0.1107
+   6 boys = 0.0201
+
+
+    p ( boys >= 3  ) = 0.6952 = 0.696 ( 3 digit approximation )
+   */
+
+}
