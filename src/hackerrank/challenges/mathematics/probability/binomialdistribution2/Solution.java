a6cb9d7145fc7416019635d3b854521a650c2a60
Remus Golgot <remus.golgot@arobs.com>
2016-07-14 15:11:52 +0300

    probability challenge binomial distribution 3

--- src/hackerrank/challenges/mathematics/probability/binomialdistribution2/Solution.java
+++ src/hackerrank/challenges/mathematics/probability/binomialdistribution2/Solution.java
@@ -9,19 +9,19 @@ package hackerrank.challenges.mathematics.probability.binomialdistribution2;
 public class Solution {
 
    /**
-   p(boy)  = 0.5215
-   p(girl) = 0.4784
+   p(boy)    = 0.5215
+   p(girl)   = 0.4784
 
-   0 boys = 0.0119
-   1 boy = 0.0784
-   2 boys = 0.2136
-   3 boys = 0.3105
-   4 boys = 0.2539
-   5 boy  = 0.1107
-   6 boys = 0.0201
+   p(0 boys) = 0.0119
+   p(1 boy)  = 0.0784
+   p(2 boys) = 0.2136
+   p(3 boys) = 0.3105
+   p(4 boys) = 0.2539
+   p(5 boys) = 0.1107
+   p(6 boys) = 0.0201
 
 
-    p ( boys >= 3  ) = 0.6952 = 0.696 ( 3 digit approximation )
+   p ( boys >= 3  ) = 0.6952 => 0.696 ( 3 digit approximation )
    */
 
 }
---
+++ src/hackerrank/challenges/mathematics/probability/binomialdistribution3/Solution.java
@@ -0,0 +1,23 @@
+package hackerrank.challenges.mathematics.probability.binomialdistribution3;
+
+/**
+ * https://www.hackerrank.com/challenges/binomial-distribution-3
+ *
+ A manufacturer of metal pistons finds that on average, 12% of his pistons are rejected because they are either oversized or undersized.
+ What is the probability that a batch of 10 pistons will contain:
+
+(a) no more than 2 rejects? = p0 + p1 + p2 = 0.891
+
+(b) at least 2 rejects? = 1 - ( p0 + p1 ) = 0.342
+ */
+
+public class Solution {
+
+    /**
+     * p ( rejected piston ) = 0.12
+     *
+     * p0 = p ( no rejected piston in a batch of 10 )        = 0.2785
+     * p1 = p ( exactly 1 rejected piston in a batch of 10 ) = 0.3797
+     * p2 = p ( exactly 2 rejected pistons in a batch of 10) = 0.2330
+     */
+}
