41e03a6c2ad3ae46e99c86c0b5a6a005bdc01f3c
Remus Golgot <remus.golgot@arobs.com>
2016-07-19 15:20:30 +0300

    solution for Sherlock and moving tiles

---
+++ src/hackerrank/challenges/mathematics/fundamentals/sherlockandmovingtiles/Solution.java
@@ -0,0 +1,36 @@
+package hackerrank.challenges.mathematics.fundamentals.sherlockandmovingtiles;
+
+import java.util.Scanner;
+
+/**
+ * https://www.hackerrank.com/challenges/sherlock-and-moving-tiles
+ *
+ Sherlock is given square tiles, initially both of whose sides have length L placed in an xOy plane,
+ so that the left bottom of each square coincides with the the origin and their sides are parallel to the axes.
+ At t=0 , both squares start moving along line y = x (along the positive x and y) with velocities s1 and s2 .
+ For each query of form qi, Sherlock has to report the time at which the overlapping area of tiles is equal to qi.
+ */
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String firstLine = scanner.nextLine();
+        int length = Integer.parseInt(firstLine.split(" ")[0]);
+        int s1 = Integer.parseInt(firstLine.split(" ")[1]);
+        int s2 = Integer.parseInt(firstLine.split(" ")[2]);
+
+        int nrTests = scanner.nextInt();
+        scanner.nextLine();
+        for (int i = 0; i < nrTests; i++) {
+            int q = Integer.parseInt(scanner.nextLine());
+            if (s1 == s2) {
+                System.out.println(-1);
+            }
+            if (q > length * length) {
+                System.out.println(-1);
+            }
+            System.out.println(Math.abs(Math.sqrt(2) * (length - Math.sqrt(q)) / (s2 - s1)));
+        }
+    }
+}
